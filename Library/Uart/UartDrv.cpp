bcd293c70d4a288243828a6a3ec98263015d3c68
Maksim Meshcheryakov <xyphrodite@gmail.com>
2025-10-02 13:59:29 +0300

    +update UartDrv and UnitControl

--- Library/Uart/UartDrv.cpp
+++ Library/Uart/UartDrv.cpp
@@ -4,12 +4,13 @@
 /// @details Заголовочный файл драйвера UartDrv
 
 #include "UartDrv.h"
+#include <string.h>
 
 /// @brief main variable of uart driver */
 UartDrv_t uartDrv;
 
 
-void drv_uart_init(void) {
+void drv_uart_init_ex(const usart_init_type* init) {
     crm_periph_clock_enable(CRM_GPIOA_PERIPH_CLOCK, TRUE);
     crm_periph_clock_enable(CRM_USART1_PERIPH_CLOCK, TRUE);
     crm_periph_clock_enable(CRM_DMA1_PERIPH_CLOCK, TRUE);
@@ -43,19 +44,26 @@ void drv_uart_init(void) {
     gpio_init_struct.gpio_pull = GPIO_PULL_NONE;
     gpio_init(DEBUG_USART_RX_GPIO_PORT, &gpio_init_struct);
 
-    /* configure param */
-    usart_init(USART1, USART1_BAUD_RATE, USART_DATA_8BITS, USART_STOP_1_BIT);
-    usart_transmitter_enable(USART1, TRUE);
-    usart_receiver_enable(USART1, TRUE);
-    usart_parity_selection_config(USART1, USART_PARITY_NONE);
+    /* configure param from arguments */
+    usart_type* us = (init != 0 && init->usart != 0) ? init->usart : USART1;
+    uint32_t baud = (init != 0) ? init->baudrate : USART1_BAUD_RATE;
+    usart_data_bit_num_type data_bits = (init != 0) ? (usart_data_bit_num_type)init->data_bit : USART_DATA_8BITS;
+    usart_stop_bit_num_type stop_bits = (init != 0) ? (usart_stop_bit_num_type)init->stop_bit : USART_STOP_1_BIT;
+    usart_parity_selection_type parity = (init != 0) ? (usart_parity_selection_type)init->parity : USART_PARITY_NONE;
+    usart_hardware_flow_control_type flow = (init != 0) ? (usart_hardware_flow_control_type)init->hardware_flow_control : USART_HARDWARE_FLOW_NONE;
 
-    usart_dma_receiver_enable(USART1, TRUE);
+    usart_init(us, baud, data_bits, stop_bits);
+    usart_transmitter_enable(us, TRUE);
+    usart_receiver_enable(us, TRUE);
+    usart_parity_selection_config(us, parity);
 
-    usart_hardware_flow_control_set(USART1, USART_HARDWARE_FLOW_NONE);
+    usart_dma_receiver_enable(us, TRUE);
 
-    usart_enable(USART1, TRUE);
+    usart_hardware_flow_control_set(us, flow);
 
-    usart_data_receive(USART1);
+    usart_enable(us, TRUE);
+
+    usart_data_receive(us);
 
     uartDrv.txCnt = 0;
     uartDrv.rx_tail_index = 0;
@@ -65,6 +73,19 @@ void drv_uart_init(void) {
     memset(uartDrv.rxDmaBuf, 0, DMA1_CHANNEL1_BUFFER_SIZE);
     memset(uartDrv.rxRingBuf, 0, RX_RING_BUF_SIZE);
     memset(uartDrv.txBuf, 0, UART_TXBUF_SIZE);
+}
+
+void drv_uart_init(void)
+{
+    usart_init_type def = {
+        .baudrate = USART1_BAUD_RATE,
+        .data_bit = USART_DATA_8BITS,
+        .stop_bit = USART_STOP_1_BIT,
+        .parity = USART_PARITY_NONE,
+        .mode = USART_MODE_TX_RX,
+        .hardware_flow_control = USART_HARDWARE_FLOW_NONE
+    };
+    drv_uart_init_ex(&def);
 }
 
 void drv_uart_transmit(uint8_t* pTxBuf, uint16_t cnt) {
@@ -84,6 +105,28 @@ void drv_uart_transmit(uint8_t* pTxBuf, uint16_t cnt) {
         }
         usart_data_transmit(USART1, uartDrv.txBuf[i]);
     }
+}
+
+
+void drv_uart_init_apply(const usart_init_type* init)
+{
+    uint32_t baud = (init != 0) ? init->baudrate : USART1_BAUD_RATE;
+    usart_data_bit_num_type data_bits = (init != 0) ? (usart_data_bit_num_type)init->data_bit : USART_DATA_8BITS;
+    usart_stop_bit_num_type stop_bits = (init != 0) ? (usart_stop_bit_num_type)init->stop_bit : USART_STOP_1_BIT;
+    usart_parity_selection_type parity = (init != 0) ? (usart_parity_selection_type)init->parity : USART_PARITY_NONE;
+    usart_hardware_flow_control_type flow = (init != 0) ? (usart_hardware_flow_control_type)init->hardware_flow_control : USART_HARDWARE_FLOW_NONE;
+
+    usart_init(USART1, baud, data_bits, stop_bits);
+    usart_transmitter_enable(USART1, TRUE);
+    usart_receiver_enable(USART1, TRUE);
+    usart_parity_selection_config(USART1, parity);
+    usart_hardware_flow_control_set(USART1, flow);
+}
+
+void drv_uart_init_adapter(void* cfg)
+{
+    const usart_init_type* init = (const usart_init_type*)cfg;
+    drv_uart_init_ex(init);
 }
 
 
--- Library/Uart/UartDrv.h
+++ Library/Uart/UartDrv.h
@@ -3,7 +3,6 @@
 /// @brief Заголовочный файл драйвера RS485 Master *
 /// @details Заголовочный файл драйвера RS485 Master
 
-
 #ifndef _DRV_UART_H_
 #define _DRV_UART_H_
 
@@ -11,73 +10,88 @@
 extern "C" {
 #endif
 
+#include "Consts.h"
+#include "Macros.h"
 #include "at32f403a_407_clock.h"
+#include "at32f403a_407_usart.h"
+#include "at32f403a_407_wk_config.h"
+#include "unitControl.h"
 #include <stdint.h>
 #include <stdio.h>
-#include "at32f403a_407_wk_config.h"
-#include <string.h>
-#include "Macros.h"
-#include "Consts.h"
-#include "unitControl.h"
 
-#define DEBUG_USART_TX_PIN    GPIO_PINS_9
-#define DEBUG_USART_TX_GPIO_PORT    GPIOA
-#define DEBUG_USART_RX_PIN    GPIO_PINS_10
-#define DEBUG_USART_RX_GPIO_PORT    GPIOA
 
-#define UART_TXBUF_SIZE (256)                  // Размер буфера передачи RS485 Master
-#define UART_RXBUF_SIZE (256)                  // Размер приёмного буфера RS485 Master, 2^n !!!
-    //#define RS485_RXBUF_MASK (RS485_RXBUF_SIZE - 1) // Маска приёмного буфера RS485 Master
+#define DEBUG_USART_TX_PIN GPIO_PINS_9
+#define DEBUG_USART_TX_GPIO_PORT GPIOA
+#define DEBUG_USART_RX_PIN GPIO_PINS_10
+#define DEBUG_USART_RX_GPIO_PORT GPIOA
 
-#define USART1_BAUD_RATE (115200U)       // Скорость RS485 Master
+#define UART_TXBUF_SIZE (256) // Размер буфера передачи RS485 Master
+#define UART_RXBUF_SIZE (256) // Размер приёмного буфера RS485 Master, 2^n !!!
+// #define RS485_RXBUF_MASK (RS485_RXBUF_SIZE - 1) // Маска приёмного буфера
+// RS485 Master
+
+#define USART1_BAUD_RATE (115200U) // Скорость RS485 Master
 #define RX_RING_BUF_SIZE (512U)
 
+/* USART mode enumeration */
+typedef enum {
+  USART_MODE_RX = 0x01,
+  USART_MODE_TX = 0x02,
+  USART_MODE_TX_RX = 0x03
+} usart_mode_type;
 
 /* Define USART init structure if not provided by vendor headers */
 #ifndef __USART_INIT_TYPE_DEFINED
 #define __USART_INIT_TYPE_DEFINED
-    typedef struct
-    {
-        uint32_t baudrate;
-        uint32_t data_bit;
-        uint32_t stop_bit;
-        uint32_t parity;
-        uint32_t mode;
-        uint32_t hardware_flow_control;
-    } usart_init_type;
+typedef struct {
+  usart_type* usart;
+  uint32_t baudrate;
+  usart_data_bit_num_type data_bit;
+  usart_stop_bit_num_type stop_bit;
+  usart_parity_selection_type parity;
+  usart_mode_type mode;
+  usart_hardware_flow_control_type hardware_flow_control;
+} usart_init_type;
 #endif
 
+/// @brief инициализация периферии МК (без параметров — использует значения по
+/// умолчанию)
+void drv_uart_init(void);
+/// @brief инициализация периферии МК с параметрами
+void drv_uart_init_ex(const usart_init_type *init);
+void drv_uart_init_adapter(void *cfg);
+void drv_uart_init_apply(const usart_init_type *init);
 
-/// @brief инициализация периферии МК
-    void drv_uart_init(void);
+/// @brief  Передаёт cnt байт данных из буфера по указателю pTxBuf в шину rs485.
+///         Запускает процесс передачи: переключение линии в передачу, отправку
+///         данных, переключение линии в приём
+/// @param [in] pTxBuf  :: указатель на буфер, данные из которого будут переданы
+/// в шину RS485
+/// @param [in] cnt     :: кол-во байт, которое необходимо передать
+void drv_uart_transmit(uint8_t *pTxBuf,
+                       uint16_t cnt); // передаёт cnt байт данных data по rs485
+// void drv_uart_transmit_str(const char* pTxBuf, uint16_t cnt);
 
-    /// @brief  Передаёт cnt байт данных из буфера по указателю pTxBuf в шину rs485.
-    ///         Запускает процесс передачи: переключение линии в передачу, отправку данных,
-    ///         переключение линии в приём
-    /// @param [in] pTxBuf  :: указатель на буфер, данные из которого будут переданы в шину RS485
-    /// @param [in] cnt     :: кол-во байт, которое необходимо передать
-    void drv_uart_transmit(uint8_t* pTxBuf, uint16_t cnt); // передаёт cnt байт данных data по rs485
-    // void drv_uart_transmit_str(const char* pTxBuf, uint16_t cnt);
+uint16_t drv_uart_read();
 
-    uint16_t drv_uart_read();
+/// @brief структура для работы драйвера RS485
+typedef struct {
+  uint8_t txBuf[UART_TXBUF_SIZE]; /// Указатель на буфер с данными, которые
+                                  /// будут отправлены в RS485
+  uint16_t txCnt; /// Кол-во байт, которые нужно отправить в RS485
+  uint8_t rxDmaBuf[DMA1_CHANNEL1_BUFFER_SIZE]; /// buffer DMA
+  uint8_t rxRingBuf[RX_RING_BUF_SIZE];
+  /** Index of tail. */
+  uint16_t rx_tail_index;
+  /** Index of head. */
+  uint16_t rx_head_index;
+  /** Index of tail. */
+  uint16_t dma_tail_index;
+  /** Index of head. */
+  uint16_t dma_head_index;
+} UartDrv_t;
 
-    /// @brief структура для работы драйвера RS485
-    typedef struct {
-        uint8_t txBuf[UART_TXBUF_SIZE];    /// Указатель на буфер с данными, которые будут отправлены в RS485
-        uint16_t txCnt;      /// Кол-во байт, которые нужно отправить в RS485
-        uint8_t rxDmaBuf[DMA1_CHANNEL1_BUFFER_SIZE];      /// buffer DMA
-        uint8_t rxRingBuf[RX_RING_BUF_SIZE];
-        /** Index of tail. */
-        uint16_t rx_tail_index;
-        /** Index of head. */
-        uint16_t rx_head_index;
-        /** Index of tail. */
-        uint16_t dma_tail_index;
-        /** Index of head. */
-        uint16_t dma_head_index;
-    } UartDrv_t;
-
-    extern UartDrv_t uartDrv;
+extern UartDrv_t uartDrv;
 
 #ifdef __cplusplus
 }
