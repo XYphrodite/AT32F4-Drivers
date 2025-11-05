44a03742189d0cde6372bae22e549f59d5b41785
Maksim Meshcheryakov <xyphrodite@gmail.com>
2025-11-05 17:53:36 +0300

    fix(watchdog): remove duplicate WatchDogSubject_t definition from wdt.h
    - Remove WatchDogSubject_t struct definition from HealthCheckWdt/wdt.h
    - Include WatchDog/WatchDogApi.h instead to use unified type
    - Fixes conflicting type declaration compile error

--- Library/ArteryCore/WatchDog/HealthCheckWdt/wdt.h
+++ Library/ArteryCore/WatchDog/HealthCheckWdt/wdt.h
@@ -11,17 +11,7 @@ extern "C" {
 #include "Tracing.h"
 #include "at32f403a_407.h"
 #include "ErrorJournal.h"
-
-
-        typedef struct WatchDogSubject {
-                bool unit_control : 1;
-                bool blinky : 1;
-                bool gateway : 1;
-                bool uart : 1;
-                bool end_node : 1;
-                bool temp : 1;
-                bool usb : 1;
-        } WatchDogSubject_t;
+#include "WatchDog/WatchDogApi.h"
 
         void wdt_init(void);
         void wdt_try_reload(void);
