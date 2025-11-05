caad43bc16fa72223aa7bce494b6cd63747d80c5
Maksim Meshcheryakov <xyphrodite@gmail.com>
2025-11-05 15:22:51 +0300

    feat: implement SystemServices bitfield for selective MCU service initialization
    - Add SystemServices struct with bitfield for service selection
    - Implement InitServices method for selective service initialization
    - Support initialization of: UpTime timer, Flash service, Unit reset, UART service, Watchdog timer, Bootloader config
    - Add usart_init_type parameter for UART service configuration

---
+++ Library/ArteryCore/SystemApi.cpp
@@ -0,0 +1,41 @@
+#include "SystemApi.h"
+#include "at32f403a_407.h"
+
+void SystemApi::Init(nvic_priority_group_type priority_group, MCU_speed clock,
+                     bool swd_over_jtag, uint32_t vector_table_offset) {
+  nvic_priority_group_config(priority_group);
+  ClockCfg::SetSpeed(clock);
+  crm_periph_clock_enable(CRM_IOMUX_PERIPH_CLOCK, TRUE);
+
+  if (swd_over_jtag) {
+    /* jtag-dp disabled and sw-dp enabled */
+    gpio_pin_remap_config(SWJTAG_GMUX_010, TRUE); // enable PB3
+  }
+
+  // set nvic
+  SCB->VTOR = reinterpret_cast<uint32_t>(&vector_table_offset);
+
+  __enable_irq();
+}
+
+error_status SystemApi::InitServices(const SystemServices_t& services) {
+    if (services.UpTimeTmr) {
+        init_UpTime_Tmr();
+    }
+    if (services.FlashService) {
+        FlashService::Init();
+    }
+    if (services.UnitReset) {
+        init_rst_units();
+    }
+    if (services.UartService) {
+        UartService::Init(services.init_config);
+    }
+    if (services.WatchdogTimer) {
+        simple_wdt_init();
+    }
+    if (services.BootloaderConfig) {
+        BootloadConfig::Init();
+    }
+    return SUCCESS;
+}
---
+++ Library/ArteryCore/SystemApi.h
@@ -0,0 +1,42 @@
+#ifndef _SYSTEMAPI_H_
+#define _SYSTEMAPI_H_
+
+#include <stdint.h>
+#include "McuClock/ClockCfg.h"
+#include "UpTime.h"
+#include "FlashService.h"
+#include "unitControl.h"
+#include "UartApi.h"
+#include "SimpleWD.h"
+#include "BootloadConfig.h"
+
+// System services bitfield - allows selective initialization of MCU services
+struct SystemServices
+{
+    bool UpTimeTmr : 1;
+    bool FlashService : 1;
+    bool UnitReset : 1;
+    bool UartService : 1;
+    bool WatchdogTimer : 1;
+    bool BootloaderConfig : 1;
+    bool reserved : 26;
+    usart_init_type* init_config;
+};
+
+typedef struct SystemServices SystemServices_t;
+
+class SystemApi
+{
+public:
+    SystemApi();
+    ~SystemApi();
+
+    static void Init(nvic_priority_group_type priority_group,
+    MCU_speed clock,
+    bool swd_over_jtag,
+    uint32_t vector_table_offset);
+
+    static error_status InitServices(const SystemServices_t& services);
+};
+
+#endif // _SYSTEMAPI_H_
